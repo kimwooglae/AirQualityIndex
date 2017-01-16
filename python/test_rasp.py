011cf2d45d738f3d6296d21bb7ded4c36dfc95ab
kimwooglae <kimwooglae@gmail.com>
2017-01-16 21:28:19 +0900

    date time

--- python/test_rasp.py
+++ python/test_rasp.py
@@ -1,5 +1,5 @@
 import serial
-import time
+import datetime
 import sys
 
 port = serial.Serial("/dev/ttyUSB0", 9600)
@@ -7,7 +7,8 @@ port = serial.Serial("/dev/ttyUSB0", 9600)
 print("Monitoring Dylos")
 while True:
     with open("data.txt", "a") as f:
-        data = time.ctime() + "," + port.readline().decode("utf-8").rstrip()
+    		
+        data = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S') + "," + port.readline().decode("utf-8").rstrip()
         f.write(data+'\n')
         print(data)
         sys.stdout.flush()
